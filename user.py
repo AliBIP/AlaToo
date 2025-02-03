4fc617fd7f0d0caaa9d8a684854d41e8a7593518
Ali <112511003+AliBIP@users.noreply.github.com>
2025-02-03 18:25:20 +0600

    New change

--- user.py
+++ user.py
@@ -1,13 +1,15 @@
 user_info = {}
-name = input(" name: ") 
-age = int(input(" age: "))  
-email = input("email: ") 
+name = input("What is the user's name? ")  
+age = int(input("What is the user's age? ")) 
+country_of_birth = input("What is the user's country of birth? ") 
+known_for = input("What is the user known for? ")  
 
 user_info["name"] = name
 user_info["age"] = age
-user_info["email"] = email
-
+user_info["country_of_birth"] = country_of_birth
+user_info["known_for"] = known_for
 
 print(f"Name: {user_info['name']}")
 print(f"Age: {user_info['age']}")
-print(f"Email: {user_info['email']}")
+print(f"Country of Birth: {user_info['country_of_birth']}")
+print(f"Known For: {user_info['known_for']}")
